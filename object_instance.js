c2a0d13dff328579801f2b8779a96645c2628c55
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-07-29 19:13:08 +0600

    added object instance

---
+++ object_instance.js
@@ -0,0 +1,45 @@
+// Creating by object instance.
+
+// let person = new Object();    // This new is a optional keyword.
+let person = Object();
+
+// person is a object. first_name is key and Naimur is key value.
+person.first_name = "Naimur"
+person.last_name = "Rahman"
+person.age = 24
+person.city = "Dhaka"
+person.isBangladeshi = true
+
+// Anonymus function
+person.getName = () =>{
+    return `My name is ${person.first_name} ${person.last_name}`;
+};
+
+console.log(person);
+// Output is: 
+// {
+//     first_name: 'Naimur',
+//     last_name: 'Rahman',
+//     age: 24,
+//     city: 'Dhaka',
+//     isBangladeshi: true,
+//     getName: [Function (anonymous)]
+// }
+
+
+let person = Object();
+
+// person is a object. first_name is key and Naimur is key value.
+person.first_name = "Naimur"
+person.last_name = "Rahman"
+person.age = 24
+person.city = "Dhaka"
+person.isBangladeshi = true
+
+// Anonymus function
+person.getName = () =>{
+    return `My name is ${person.first_name} ${person.last_name}`;
+};
+
+console.log(person.first_name);
+// Output is: Naimur
