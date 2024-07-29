f81a774da2b92321fcef16fa18f59953909b1302
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-07-29 20:14:00 +0600

    added class constructor

---
+++ class/class_constructor.js
@@ -0,0 +1,14 @@
+// By Creating a constructor
+
+class Person{
+    // Constructor name should be constructor
+    // Constructor execute automatically when object is created
+    // Consturctor can take parameter
+    // Constructor mathod can't return any result
+    constructor(){
+        console.log("This is constructor mathod");
+    }
+}
+
+let PersonObj =new Person();    // This new key word must be applyed.
+// Output is: This is constructor mathod
