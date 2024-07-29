048be001cd86a36ed46fc5122feca49918a9fb5b
NaimurRahmanNishat <naimurrahmun34@gmail.com>
2024-07-30 04:30:28 +0600

    added getter and setter

---
+++ class/getter_setter.js
@@ -0,0 +1,55 @@
+// By creating a setter and getter function.
+
+class Product{
+    // set means keywords.
+    set SetPrice(value){
+        this.price = value;
+    }
+
+    // get means keywords.
+    get GetPrice(){
+        return this.price;
+    }
+}
+
+let ProductObj = new Product();
+console.log(ProductObj);
+// Output is: Product {}
+
+
+
+class Product{
+    // set means keywords.
+    set SetPrice(value){
+        this.price = value;
+    }
+
+    // get means keywords.
+    get GetPrice(){
+        return this.price;
+    }
+}
+
+let ProductObj = new Product();
+ProductObj.SetPrice = 100;
+console.log(ProductObj);
+// Output is: Product { price: 100 }
+
+
+
+class Product{
+    // set means keywords.
+    set SetPrice(value){
+        this.price = value;
+    }
+
+    // get means keywords.
+    get GetPrice(){
+        return this.price;
+    }
+}
+
+let ProductObj = new Product();
+ProductObj.SetPrice = 100;
+console.log(ProductObj.GetPrice);
+// Output is: 100
